580189af9810da50f881f6fedc5d929be6da77eb
RyanFlachman <ryan@flachman.net>
2019-08-22 13:55:38 -0600

    Unskip tests

--- app.test.js
+++ app.test.js
@@ -15,12 +15,11 @@ describe('Server', () => {
   })
 })
 describe('API', () => {
-  // beforeEach(() => {
-  //   cmd.run('knex seed:run --env=test')
-
-  // })
+  beforeEach(() => {
+    cmd.run('knex seed:run --env=test')
+  })
   describe('GET /api/v1/projects', () => {
-    xit('should return a status of 200 and all projects', async () => {
+    it('should return a status of 200 and all projects', async () => {
       const expectedProjects = await database('projects').first()
         .then(project => [project.title, project.id])
 
@@ -33,7 +32,7 @@ describe('API', () => {
   })
 
   describe('GET /api/v1/palettes', () => {
-    xit('should return a status of 200 and all palettes', async () => {
+    it('should return a status of 200 and all palettes', async () => {
       const expectedPallete = await database('palettes').first()
         .then(palette => [palette.id, palette.project_id, palette.color_1, palette.color_2, palette.color_3, palette.color_4, palette.color_5])
       const response = await request(app).get('/api/v1/palettes')
@@ -46,7 +45,7 @@ describe('API', () => {
   })
 
   describe('GET /api/v1/projects/:id', () => {
-    xit('should return a status of 200 and one project by its id', async () => {
+    it('should return a status of 200 and one project by its id', async () => {
       const expectedProjectId = await database('projects').first()
         .then(project => project.id)
       
@@ -57,7 +56,7 @@ describe('API', () => {
       expect(project).toEqual(expectedProjectId)
     })
 
-    xit('should return a 404 error if cannot find project by id', async () => {
+    it('should return a 404 error if cannot find project by id', async () => {
       const expected = 'ERROR: Cannot find project id'
 
       const response = await request(app).get('/api/v1/projects/4')
@@ -111,7 +110,7 @@ describe('API', () => {
   })
 
   describe('PATCH /api/v1/palettes/:id', () => {
-    xit('should return a 422 status if a property is missing in the patch', async () => {
+    it('should return a 422 status if a property is missing in the patch', async () => {
       const { id } = await database('palettes').first('id');
       const requestBody = {};
 
@@ -121,7 +120,7 @@ describe('API', () => {
       expect(response.body.error).toEqual(`Your new project was not updated. You are missing the  property`)
     })
 
-    xit('should return a 202 status if palette has been updated', async () => {
+    it('should return a 202 status if palette has been updated', async () => {
       const { id } = await database('palettes').first('id');
       const requestBody = {color_1:'#ffffff'};
 
@@ -142,14 +141,14 @@ describe('API', () => {
   })
 
   describe('DELETE /projects/:id', () => {
-    xit('should return a 204 error and the delete project', async () => {
+    it('should return a 204 error and the delete project', async () => {
       const { id } = await database('projects').first('id');
       const response = await request(app).delete(`/api/v1/projects/${id}`);
 
       expect(response.status).toBe(204)
     })
 
-    xit('should return a 404 error if a project was not deleted', async () => {
+    it('should return a 404 error if a project was not deleted', async () => {
       const response = await request(app).delete('/api/v1/project/0');
 
       expect(response.status).toBe(404);
@@ -157,14 +156,14 @@ describe('API', () => {
   })
 
   describe('DELETE /palettes/:id', () => {
-    xit('should return a 204 and the delete palette', async () => {
+    it('should return a 204 and the delete palette', async () => {
       const { id } = await database('palettes').first('id');
       const response = await request(app).delete(`/api/v1/palettes/${id}`);
 
       expect(response.status).toBe(204)
     })
 
-    xit('should return a 404 error if a palette was not deleted', async () => {
+    it('should return a 404 error if a palette was not deleted', async () => {
       const response = await request(app).delete('/api/v1/palettes/0');
 
       expect(response.status).toBe(404);
